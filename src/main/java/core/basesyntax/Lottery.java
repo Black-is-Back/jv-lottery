789b9d035bd598e954a933044a6d5692d91bf1d5
vadym-bondar-g <evansfalter@gmail.com>
2026-06-28 12:55:17 +0200

    fix: deleted magic number

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,8 +6,9 @@ public class Lottery {
     public Ball getRandomBall() {
         Random random = new Random();
         Color [] colors =  Color.values();
+        final int maxNumber = 100;
         int randomColor = random.nextInt(colors.length);
-        int number = random.nextInt(100);
+        int number = random.nextInt(maxNumber);
         return new Ball(colors[randomColor].name(),number);
     }
 }
