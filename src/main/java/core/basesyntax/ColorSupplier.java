af5dc716d4bf92efd29f374d7bff017fa3cc91cc
vadym-bondar-g <evansfalter@gmail.com>
2026-06-28 13:07:47 +0200

    fix spell check

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -2,12 +2,11 @@ package core.basesyntax;
 
 import java.util.Random;
 
-
 public class ColorSupplier {
     public String getRandomColor() {
         Random random = new Random();
-        Color [] colors =  Color.values();
-        int index = random.nextInt (colors.length);
-        return  colors[index].name();
+        Color[] colors = Color.values();
+        int index = random.nextInt(colors.length);
+        return colors[index].name();
     }
 }
