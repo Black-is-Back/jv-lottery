72eb1e8f60878638b08f8ded33cf73c91dd38140
vadym-bondar-g <evansfalter@gmail.com>
2026-06-28 13:16:48 +0200

    fix v4

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -20,12 +20,13 @@ public class Ball {
         this.number = number;
     }
 
-    public Ball(int number, String color) {
+    public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
 
     @Override
+
     public String toString() {
         return "Ball{" + "color='" + color + '\'' + ", number=" + number + '}';
     }
